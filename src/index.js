577783bb06001de508d9c8a7c798493472ce1472
Marlou Amada <marlou@bluehive.com.ph>
2024-02-15 07:47:06 +0800

    set up backend and connect to db

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
+// import userRouter from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+
+//access variable in dotenv file
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+//db run fucntion
+connectDB();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+// app.use("/api/users", userRouter);
+
+// error Handler
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
